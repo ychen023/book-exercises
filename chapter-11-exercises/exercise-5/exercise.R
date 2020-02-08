e9dff96823299d97dab363021a41b025d9f952e3
Yilin Chen <yicchen805@gmail.com>
2020-02-07 22:00:07 -0800

    Week 5

--- chapter-11-exercises/exercise-5/exercise.R
+++ chapter-11-exercises/exercise-5/exercise.R
@@ -5,27 +5,45 @@
 #install.packages("nycflights13")  # should be done already
 library("nycflights13")
 library("dplyr")
+View(flights)
 
 # What was the average departure delay in each month?
 # Save this as a data frame `dep_delay_by_month`
 # Hint: you'll have to perform a grouping operation then summarizing your data
+dep_delay_by_month <- flights %>%
+  group_by(month) %>% 
+  summarize(
+    delay = mean(dep_delay, na.rm = TRUE)
+    )
+dep_delay_by_month
 
 
 # Which month had the greatest average departure delay?
-
+filter(dep_delay_by_month, delay == max(delay)) %>% select(month)
 
 # If your above data frame contains just two columns (e.g., "month", and "delay"
 # in that order), you can create a scatterplot by passing that data frame to the
 # `plot()` function
-
+plot(dep_delay_by_month)
 
 # To which destinations were the average arrival delays the highest?
 # Hint: you'll have to perform a grouping operation then summarize your data
 # You can use the `head()` function to view just the first few rows
+flights %>%
+  group_by(dest) %>%
+  summarise(delay = mean(arr_delay, na.rm = TRUE)) %>%
+  arrange(-delay) %>%
+  head()
 
 
 # You can look up these airports in the `airports` data frame!
-
+airports %>%
+  filter(faa == "CAE")
 
 # Which city was flown to with the highest average speed?
-
+flights %>%
+  group_by(dest) %>%
+  summarize(
+    avg_speed = mean(distance / air_time, na.rm=TRUE)
+  ) %>% 
+  filter (avg_speed == max(avg_speed, na.rm=TRUE))
--- chapter-14-exercises/exercise-1/exercise.R
+++ chapter-14-exercises/exercise-1/exercise.R
@@ -2,54 +2,60 @@
 
 # Load the httr and jsonlite libraries for accessing data
 # You can also load `dplyr` if you wish to use it
-
+library(httr)
+library(jsonlite)
+library(dplyr)
 
 # Create a variable base_uri that stores the base URI (as a string) for the 
 # Github API (https://api.github.com)
-
+base_uri <- "https://api.github.com"
 
 # Under the "Repositories" category of the API documentation, find the endpoint 
 # that will list _repos in an organization_. Then create a variable named
 # `org_resource` that stores the endpoint for the `programming-for-data-science`
 # organization repos (this is the _path_ to the resource of interest).
-
+endpoint <- "/repos/organization"
+org_resource <- paste0(base_uri, endpoint)
 
 # Send a GET request to this endpoint (the `base_uri` followed by the 
 # `org_resource` path). Print the response to show that your request worked. 
 # (The listed URI will also allow you to inspect the JSON in the browser easily).
-
+response <- GET(org_resource)
+print(respond)
 
 # Extract the content of the response using the `content()` function, saving it
 # in a variable.
-
+response_text <- content(response, type = "text")
 
 # Convert the content variable from a JSON string into a data frame.
-
+items <- response$items
 
 # How many (public) repositories does the organization have?
-
+length(org_resource)
 
 # Now a second query:
 # Create a variable `search_endpoint` that stores the endpoint used to search 
 # for repositories. (Hint: look for a "Search" endpoint in the documentation).
-
+search_endpoint <- "/search/repositories"
 
 # Search queries require a query parameter (for what to search for). Create a 
 # `query_params` list variable that specifies an appropriate key and value for 
 # the search term (you can search for anything you want!)
-
+query_params <- "q=dplyr&sort=forks"
 
 # Send a GET request to the `search_endpoint`--including your params list as the
 # `query`. Print the response to show that your request worked.
-
+reponse <- GET(paste0(base_uri, search_endpoint,query_params))
 
 # Extract the content of the response and convert it from a JSON string into a
 # data frame. 
-
+response <- GET(paste0(base_uri, search_endpoint, query_params))
 
 # How many search repos did your search find? (Hint: check the list names to 
 # find an appropriate value).
-
+response_text <- content(response, type = "text" )
+response_data <- fromJSON(response_text)
+items <- response_data$items 
 
 # What are the full names of the top 5 repos in the search results?
-
+str(response_data)
